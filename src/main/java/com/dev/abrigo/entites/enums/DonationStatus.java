b124c80f6f8b268c0820d006bc4ec0ce581f147d
Mayara <mayaradepaula1997>
2024-09-01 13:46:47 -0300

    Creation of the donation status and species enums

---
+++ src/main/java/com/dev/abrigo/entites/enums/DonationStatus.java
@@ -0,0 +1,8 @@
+package com.dev.abrigo.entites.enums;
+
+public enum DonationStatus {
+
+    AVAILABLE,          //DISPONIVEL
+    ADOPTED,            //ADOTADO
+    UNDER_TREATMENT     //EM TRATAMENTO
+}
---
+++ src/main/java/com/dev/abrigo/entites/enums/SpeciesType.java
@@ -0,0 +1,9 @@
+package com.dev.abrigo.entites.enums;
+
+public enum SpeciesType {
+
+    MAMMAL,        //MAMIFERO
+    BIRD,         //AVE
+    REPTILE,     //REPTIL
+    FISH        //PEIXE
+}
