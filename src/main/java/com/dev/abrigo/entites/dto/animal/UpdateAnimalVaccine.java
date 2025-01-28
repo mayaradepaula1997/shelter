974158007b2506475ecb7370782e7f0d20f13c27
Mayara <mayaradepaula1997>
2025-01-27 21:01:38 -0300

    Vaccine DTOs

---
+++ src/main/java/com/dev/abrigo/entites/dto/animal/UpdateAnimalVaccine.java
@@ -0,0 +1,7 @@
+package com.dev.abrigo.entites.dto.animal;
+
+import com.dev.abrigo.entites.VaccineApplied;
+import com.dev.abrigo.entites.enums.DonationStatus;
+
+public record UpdateAnimalVaccine(Long idVaccine) {
+}
---
+++ src/main/java/com/dev/abrigo/entites/dto/vaccine/UpdateVaccine.java
@@ -0,0 +1,4 @@
+package com.dev.abrigo.entites.dto.vaccine;
+
+public record UpdateVaccine(String name, String description) {
+}
